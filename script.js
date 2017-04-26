21e668d3dc3d3916cbb4c41efdcc653430b2ea1f
Dorota Buraczyńska <dorota.buraczynska@gmail.com>
2017-04-26 21:48:46 +0200

    add slider

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $('button.menu').on('click', function () {
     $('.small-menu').toggleClass('invisible');
 });
+
+$('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    autoplay: true,
+    arrows: false,
+    centerPadding: 0
+});
+
